760206850e3cd2803ef4889dbb2a8e894e187b04
Mohamed <mohamedennoun@gmail.com>
2019-03-13 11:22:35 +0100

    add favicon

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
+const favicon = require('express-favicon');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,6 +13,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
+app.use(favicon(__dirname + '/images/favicon.png'));
 
 app.use(logger('dev'));
 app.use(express.json());
