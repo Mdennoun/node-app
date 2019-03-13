7071c2106b7ceb273aa2a57730e23282f0ad681a
Mohamed <mohamedennoun@gmail.com>
2019-03-13 14:39:58 +0100

    Add /ville to add ville value and show it to user

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node App' });
+
+  res.render('index', { title: 'Welcome to Node App' })
+ 
+});
+router.get('/ville', function(req, res, next) {
+  var params = req.query.nom_ville; 
+  res.render('ville', { villeName: params});
+  console.log("Mon parametre est : " + params);
+ 
 });
 
 module.exports = router;
